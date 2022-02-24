284a0967b46fdc6cf0e5d30fb2eb5de836f3d29f
Delyar Tabatabai <tabatase@oregonstate.edu>
2022-02-23 20:35:05 -0800

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,22 @@
 -->
 <script>
 	import { onMount } from "svelte";
-	import { scaleLinear } from "d3-scale";
+	import { scaleLinear, scaleOrdinal } from "d3-scale";
+	import { schemeCategory10 } from "d3-scale-chromatic";
+
+	let colorScale;
 
 	let instances;
 
 	const numClasses = 10;
 	const numBins = 10;
 	let binsByClasses = [];
+
+	let selectedPoint;
+	let imgPath = "";
+	let selectedTrueLabel = ""
+	let selectedPrediction = ""
+	let selectedConfidence = 0
 
 	onMount(async () => {
 		const fetched = await fetch("static/prediction_results.json");
@@ -25,15 +34,15 @@
 			}
 			binsByClasses.push({"class": k, "bins": binsForClass});
 		}
-		console.log(binsByClasses);
+		console.log('Bins By Classes', binsByClasses);
+
+		colorScale = scaleOrdinal(schemeCategory10)
+			.domain(instances.map(row => row["true_label"]));
 
 		instances.forEach(instance => {
-			
+			// console.log('projection x', instance.projection[0])
 
 		});
-
-		
-
 	});
 
 	
@@ -41,22 +50,46 @@
 </script>
 
 <main>
-	<h1>Visual Analytics HW 3</h1>
+	<h1>Visual Analytics HW 3 - Delyar Tabatabai</h1>
 
 	<div id="container">
 		<div id="sidebar" style="width: 450px;">
 			<div id="projection-view" class="view-panel">
 				<div class="view-title">Projection View</div>
-				<svg >
-					
+				<svg id="scatterplot-container">
+					{#if instances !== undefined}	
+						<g id="scatterplotData" transform="translate({50}, {20})">
+							{#each instances as record}
+								<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+								<circle 
+									id="datapoint-{record.id}"
+									class="unselected-circle"
+									cx={record.projection[0]*4.5 + 100}
+									cy={record.projection[1]*3.5 + 62} 
+									r="4"
+									style="fill: {colorScale(record["true_label"])}; stroke: {colorScale(record["predicted_label"])}"
+									on:click={()=>{
+										selectedPoint = record["id"];
+										imgPath = "static/images/" + instances[record["id"]].filename
+										selectedTrueLabel = instances[record["id"]].true_label
+										selectedPrediction = instances[record["id"]].predicted_label
+									}}
+								> <title>{record["true_label"]}</title></circle>
+							{/each}
+						</g>
+					{/if}
 
 				</svg>
 			</div>
 			<div id="selected-image-view" class="view-panel">
 				<div class="view-title">Selected Image</div>
 				<div id="selected-image-view-content">
-
-
+					<img style="height:90px; width: 90px; float: left" src={imgPath} alt="No points selected">
+					<div style="float: right">
+						<p>ID: {selectedPoint}</p>
+						<p>Labled as {selectedTrueLabel}</p>
+						<p>Predicted as {selectedPrediction} (Confidence: {selectedConfidence})</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -88,8 +121,12 @@
 	}
 	.view-panel {
 		border: 2px solid #eee;
-		margin-bottom: 15px;
-		margin-right: 15px;
+		margin-bottom: 10px;
+		margin-right: 10px;
+	}
+	.unselected-circle {
+		stroke-width: 1;
+		fill-opacity: 0.3;
 	}
 	.view-title {
 		background-color: #f3f3f3;
